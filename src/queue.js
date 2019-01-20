c7c31bbb02ddea174b06b7c59ee1944b86011564
Subash Pathak <subash@subash.me>
2019-01-20 11:57:29 +0545

    Initial commit

---
+++ src/queue.js
@@ -0,0 +1,155 @@
+const { Emitter } = require('event-kit');
+
+class Queue {
+  constructor({ automatic = true, concurrency = 1, timeout } = {}) {
+    this.emitter = new Emitter();
+    this.paused = false;
+    this.destroyed = false;
+    this.automatic = automatic;
+    this.concurrency = concurrency;
+    this.timeout = timeout;
+    this.runningCount = 0;
+    this.tasks = [];
+  }
+
+  _add(task) {
+    if(typeof task !== 'function') throw new Error(`Task must be a function`);
+    this.tasks.push(task);
+    this.emitter.emit('did-add', task);
+  }
+
+  add(...tasks) {
+    tasks.forEach( task=> this._add(task));
+    if(!this.automatic) return;
+
+    //Run on the next tick to allow consumers to set observers after adding tasks
+    process.nextTick(this.next.bind(this));
+  }
+
+  remove(task) {
+    if(!this.tasks.includes(task)) return;
+    this.tasks.splice(this.tasks.indexOf(task), 1);
+    this.emitter.emit('did-remove', task);
+  }
+
+  next() {
+    if(this.paused || this.destroyed) return;
+    if(this.runningCount >= this.concurrency) return;
+
+    //Fire finish event if there are no tasks left
+    if(!this.tasks.length) return this.emitter.emit('did-finish');
+
+    //Get a task from queue then run it
+    //this.runTask never rejects
+    this.runTask(this.tasks.shift()).then(()=> this.next());
+
+    //Call next to run multiple tasks concurrently
+    this.next();
+  }
+
+  _runWithTimeout(task) {
+    return new Promise((resolve, reject)=> {
+      const timer = setTimeout(()=> {
+        const err = new Error('Timeout');
+        err.code = 'ETIMEDOUT';
+        reject(err);
+      }, this.timeout);
+
+      Promise.resolve()
+        .then(task)
+        .then((result)=> {
+          clearTimeout(timer);
+          resolve(result);
+        })
+        .catch((err)=> {
+          clearTimeout(timer);
+          reject(err);
+        });
+    });
+  }
+
+  async runTask(task) {
+    try {
+      this.runningCount++;
+      this.emitter.emit('will-run', task);
+      const result = await (this.timeout? this._runWithTimeout(task): task());
+      this.emitter.emit('did-run', { task , result });
+    } catch (error) {
+      this.emitter.emit('did-fail', { task, error });
+    } finally {
+      this.runningCount--;
+    }
+  }
+
+  pause() {
+    this.paused = true;
+    this.emitter.emit('did-pause');
+  }
+
+  resume() {
+    this.paused = false;
+    this.next();
+    this.emitter.emit('did-resume');
+  }
+
+  start() {
+    this.resume();
+  }
+
+  isPaused() {
+    return this.paused;
+  }
+
+  clear() {
+    this.tasks = [];
+  }
+
+  get size() {
+    return this.tasks.length + this.runningCount;
+  }
+
+  onDidAdd(callback) {
+    return this.emitter.on('did-add', callback);
+  }
+
+  onDidRemove(callback) {
+    return this.emitter.on('did-remove', callback);
+  }
+
+  onWillRun(callback) {
+    return this.emitter.on('will-run', callback);
+  }
+
+  onDidRun(callback) {
+    return this.emitter.on('did-run', callback);
+  }
+
+  onDidFail(callback) {
+    return this.emitter.on('did-fail', callback);
+  }
+
+  onDidPause(callback) {
+    return this.emitter.on('did-pause', callback);
+  }
+
+  onDidResume(callback) {
+    return this.emitter.on('did-resume', callback);
+  }
+
+  onDidFinish(callback) {
+    return this.emitter.on('did-finish', callback);
+  }
+
+  onWillDestroy(callback) {
+    return this.emitter.on('will-destroy', callback);
+  }
+
+  destroy() {
+    this.emitter.emit('will-destroy');
+    this.destroyed = true;
+    this.clear();
+    this.emitter.dispose();
+  }
+}
+
+module.exports = Queue;
---
+++ tests/queue.test.js
@@ -0,0 +1,136 @@
+const Queue = require('../src/queue');
+
+test('Test onWillDestroy()', (done)=> {
+  const queue = new Queue();
+  queue.onWillDestroy(done);
+  queue.destroy();
+  queue.add(jest.fn()); //Should do nothing
+});
+
+test('Test add() and onDidAdd()', (done)=> {
+  const queue = new Queue();
+  const task = ()=> {};
+  queue.onDidAdd((_task)=> {
+    expect(_task).toBe(task);
+    done();
+  });
+  queue.add(task, task, task, task, task);
+  expect(queue.size).toBe(5);
+});
+
+test('Test timeout', (done)=> {
+  const queue = new Queue({ timeout: 500 });
+  const task = ()=> new Promise((resolve)=>{ setTimeout(resolve, 600)});
+  queue.add(task);
+  queue.onDidFail(({ error })=> {
+    expect(error.code).toBe('ETIMEDOUT');
+    queue.destroy();
+    done();
+  });
+});
+
+test('Test _runWithTimeout() error', (done)=> {
+  const queue = new Queue({ timeout: 500 });
+  const task = jest.fn().mockRejectedValue(new Error('Hello World'));
+  queue.add(task);
+  queue.onDidFail(({ error })=> {
+    expect(error.message).toBe('Hello World');
+    queue.destroy();
+    done();
+  });
+});
+
+test('Test clear()', (done)=> {
+  const queue = new Queue();
+  const task = jest.fn();
+  queue.onDidRun(()=> queue.clear()); //Clear after first run
+  queue.add(task, task, task, task, task);
+  queue.onDidFinish(()=> {
+    expect(task).toHaveBeenCalledTimes(1);
+    queue.destroy();
+    done();
+  });
+});
+
+test('Test onDidAdd() error', ()=> {
+  const queue = new Queue();
+  try {
+    queue.add('abcd');
+    expect(true).toBe(false);
+  } catch (err) {
+    expect(err.message).toBe('Task must be a function');
+  }
+  queue.destroy();
+});
+
+test('Test remove() and onDidRemove()', (done)=> {
+  const queue = new Queue({ automatic: false });
+  queue.onDidRemove(()=> done());
+  const task = ()=> {};
+  queue.add(task);
+  queue.remove(task);
+  queue.remove(task);
+  queue.destroy();
+});
+
+test('Test pause() and onDidPause()', (done)=> {
+  const queue = new Queue();
+  queue.onDidPause(()=> done());
+  queue.pause();
+  expect(queue.isPaused()).toBe(true);
+  queue.destroy();
+});
+
+test('Test resume() and onDidResume()', (done)=> {
+  const queue = new Queue();
+  queue.onDidResume(()=> done());
+  queue.pause();
+  queue.start(); //Start is just an alias for resume()
+  queue.destroy();
+});
+
+test('Test onWillRun()', (done)=> {
+  const queue = new Queue();
+  const task = jest.fn();
+  queue.onWillRun((_task)=> {
+    expect(_task).toBe(task);
+    queue.destroy();
+    done();
+  });
+  queue.add(task);
+});
+
+test('Test onDidRun()', (done)=> {
+  const queue = new Queue({ timeout: 500 });
+  const task = jest.fn().mockResolvedValue('hello world');
+  queue.onDidRun((result)=> {
+    expect(result.task).toBe(task);
+    expect(result.result).toBe('hello world');
+    queue.destroy();
+    done();
+  });
+  queue.add(task);
+});
+
+test('Test onDidFail()', (done)=> {
+  const queue = new Queue();
+  const task = jest.fn().mockRejectedValue(new Error('hello world'));
+  queue.onDidFail((result)=> {
+    expect(result.task).toBe(task);
+    expect(result.error.message).toContain('hello world');
+    queue.destroy();
+    done();
+  });
+  queue.add(task);
+});
+
+test('Test onDidFinish()', (done)=> {
+  const queue = new Queue();
+  const task = jest.fn();
+  queue.onDidFinish(()=> {
+    expect(task).toHaveBeenCalledTimes(5);
+    queue.destroy();
+    done();
+  });
+  queue.add(task, task, task, task, task);
+});
